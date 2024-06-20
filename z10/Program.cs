4c67b9a2396131c297a6392ef90e84b1943116ec
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 18:08:40 +0300

    Добавьте файлы проекта.

---
+++ z10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static int SumOf2DArray(int[,] array)
+    {
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+
+    static void Main()
+    {
+        int[,] twoDArray = { { 1, 2, 3 }, { 4, 5, 6 } };
+        Console.WriteLine($"Сумма всех элементов двумерного массива: {SumOf2DArray(twoDArray)}");
+    }
+}
